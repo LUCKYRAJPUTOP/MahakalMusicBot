02e31c266e2ee53e5fdbcfa4837fc926ce518f7c
Anshul Rajput <82425167+LUCKYRAJPUTOP@users.noreply.github.com>
2021-06-08 08:03:33 +0530

    Update chat_member_updated.py

--- CodentsMusic/modules/chat_member_updated.py
+++ CodentsMusic/modules/chat_member_updated.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated
 
-from CodentsMusic.function import *
+from MahakalMusic.function import *
 
 
 @Client.on_chat_member_updated()
