50ff0221c650243c8975df4e2f15a16d4f042520
Anshul Rajput <82425167+LUCKYRAJPUTOP@users.noreply.github.com>
2021-06-08 08:11:39 +0530

    Update private.py

--- CodentsMusic/modules/private.py
+++ CodentsMusic/modules/private.py
@@ -1,4 +1,4 @@
-# CodentsMusic (Telegram bot project )
+# MahakalMusic (Telegram bot project )
 # Copyright (C) 2021  Inukaasith
 
 # This program is free software: you can redistribute it and/or modify
@@ -15,10 +15,10 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
-from CodentsMusic.modules.msg import Messages as tr
+from MahakalMusic.modules.msg import Messages as tr
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from CodentsMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
+from MahakalMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
 logging.basicConfig(level=logging.INFO)
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['start']))
@@ -31,6 +31,9 @@ def _start(client, message):
                 [
                     InlineKeyboardButton(
                         "➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+                [
+                    InlineKeyboardButton(
+                        "𝕮𝖗𝖊𝖆𝖙𝖔𝖗 ✌️", url=f"https://t.me/Ho_Iz_Lucky")],
                 [
                     InlineKeyboardButton(
                         "📲 Updates", url=f"https://t.me/{UPDATES_CHANNEL}"), 
